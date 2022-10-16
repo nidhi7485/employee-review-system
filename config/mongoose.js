f513230b4f7aa62570ec8d19f743043fdbcc0a2c
nidhi <nidhi7485@gmail.com>
2022-10-16 17:40:36 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+// connect from mongodb
+// mongoose.connect('mongodb://localhost:27017/Review-System');
+mongoose.connect(
+  'mongodb+srv://nidhi:12345@cluster0.3j2femv.mongodb.net/?retryWrites=true&w=majority'
+)
+
+// aquire connection if it is succesful
+const db = mongoose.connection
+
+// error
+db.on('error', console.error.bind(console, 'error connecting to db'))
+
+// up and running then print the message
+db.once('open', function () {
+  console.log('successfully connected to database!')
+})
+
+module.exports = db
